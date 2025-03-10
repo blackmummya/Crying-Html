31d6d6def17d2fdf9d6bccb2c2a06d2e096cfe38
MUMMYA <108289898+blackmummya@users.noreply.github.com>
2025-03-10 21:24:43 +0300

    Update banner.py

--- banner.py
+++ banner.py
@@ -8,8 +8,8 @@ def banner():
           f'       ({c.ANIMATION + c.GREEN}({c.RESET})'
           f'{c.ANIMATION + c.GREEN}({c.RESET}       (     '
           f'              ')
-    print(f'  ){c.ANIMATION + c.GREEN}\){c.RESET})       '
-          f'){c.ANIMATION + c.GREEN}){c.RESET}\       )\
+    print(f'  ){c.ANIMATION + c.GREEN}\\){c.RESET})       '
+          f'){c.ANIMATION + c.GREEN}){c.RESET}\\       )\\
           ( {c.ANIMATION + c.GREEN}({c.RESET}   /)     '
           f' ){c.ANIMATION + c.GREEN}){c.RESET} ')
     print(f' ({c.ANIMATION + c.GREEN}()/{c.RESET}(     '
@@ -19,16 +19,16 @@ def banner():
           f'({c.ANIMATION + c.GREEN}({c.RESET}      '
           f'(/{c.ANIMATION + c.GREEN}({c.RESET}( ')
     print(f' (_{c.ANIMATION + c.GREEN}){c.RESET}){c.ANIMATION + c.GREEN}'
-          f'({c.RESET}    )\({c.ANIMATION + c.GREEN}){c.RESET}) '
-          f'){c.ANIMATION + c.GREEN}){c.RESET}\    ) {c.ANIMATION + c.GREEN}'
+          f'({c.RESET}    )\\({c.ANIMATION + c.GREEN}){c.RESET}) '
+          f'){c.ANIMATION + c.GREEN}){c.RESET}\\    ) {c.ANIMATION + c.GREEN}'
           f'({c.RESET} /(    ){c.ANIMATION + c.GREEN}){c.RESET}'
-          f')\ {c.ANIMATION + c.GREEN}){c.RESET}(    \(_{c.ANIMATION + c.GREEN}){c.RESET}(    ')
+          f')\\ {c.ANIMATION + c.GREEN}){c.RESET}(    \\(_{c.ANIMATION + c.GREEN}){c.RESET}(    ')
     print(f'{c.ANIMATION + c.GREEN}({c.RESET}_){c.ANIMATION + c.GREEN}'
           f'){c.RESET}_){c.ANIMATION + c.GREEN}){c.RESET}  ({c.ANIMATION + c.GREEN}'
           f'({c.RESET}_){c.ANIMATION + c.GREEN}){c.RESET}/ /({c.ANIMATION + c.GREEN}'
-          f'({c.RESET}_)  (({c.ANIMATION + c.GREEN}){c.RESET} \(  ){c.ANIMATION + c.GREEN}'
+          f'({c.RESET}_)  (({c.ANIMATION + c.GREEN}){c.RESET} \\(  ){c.ANIMATION + c.GREEN}'
           f'){c.RESET}_|({c.ANIMATION + c.GREEN}({c.RESET}_)({c.ANIMATION + c.GREEN}'
-          f'({c.RESET}    )\){c.ANIMATION + c.GREEN}){c.RESET}_   ')
+          f'({c.RESET}    )\\){c.ANIMATION + c.GREEN}){c.RESET}_   ')
     print(f'╭━━╮╭╮{c.ANIMATION + c.RED}╱╱{c.RESET}╭━━━┳━━━┳╮╭━╮ ╭━╮╭━┳╮{c.ANIMATION + c.RED}'
           f'╱{c.RESET}╭┳━╮╭━┳━╮╭━┳╮{c.ANIMATION + c.RED}╱╱{c.RESET}╭┳━━━╮')
     print(f'┃╭╮┃┃┃{c.ANIMATION + c.RED}╱╱{c.RESET}┃╭━╮┃╭━╮┃┃┃╭╯ ┃┃╰╯┃┃┃{c.ANIMATION + c.RED}╱'
